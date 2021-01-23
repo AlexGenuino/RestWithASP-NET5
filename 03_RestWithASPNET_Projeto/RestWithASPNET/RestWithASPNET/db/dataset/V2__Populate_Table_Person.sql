5e66442871ec37c6f3e80198c4135770f20d007f
Alexsander <genuinoalexsander@gmail.com>
2021-01-23 19:22:57 -0300

    projeto - migartions

---
+++ 03_RestWithASPNET_Projeto/RestWithASPNET/RestWithASPNET/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (1,'São Paulo - Brasil','Alexsander','Male','Genuino');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (2,'São pAULO - Brasil','Leonardo','Male','Costa');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (3,'Rio de Janeiro - Brasil','Julio','Male','Pereira');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (4,'Mato Grosso - Brasil','Brito','Male','Junior');
